211cb9e79b7833ba3a6cda814d83f7d2cb562ddf
Natália Rodrigues <nataliamrq@gmail.com>
2022-08-03 10:35:24 -0300

    Adding environment variables.

--- src/api/search.js
+++ src/api/search.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
 
-const API_KEY = 'AIzaSyC91PSj-Cs8Vw62lMiGTRNkPXmXT12ceo0';
-
 export async function search(searchTerm) {
     const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
       params: {
-        key: API_KEY,
+        key: process.env.VUE_APP_API_KEY,
         type: 'video',
         part: 'snippet',
         q: searchTerm
